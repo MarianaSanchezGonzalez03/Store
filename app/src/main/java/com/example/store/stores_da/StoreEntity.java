f91fecffc9aa5df1d30d0545d6fb19bfb5ac3248
Mariana Sanchez Gonzalez <mari98san33@gmail.com>
2021-12-12 00:51:14 -0600

    Equal & HashCode

--- app/src/main/java/com/example/store/stores_da/StoreEntity.java
+++ app/src/main/java/com/example/store/stores_da/StoreEntity.java
@@ -2,9 +2,24 @@ package com.example.stores_da
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 @Entity(tableName = "StoreEntity")
-data class StoreEntity(@PrimaryKey(autoGenerate = true) var id: Long = 0,
+data class  StoreEntity(@PrimaryKey(autoGenerate = true) var id: Long = 0,
                        var name: String,
                        var phone: String = "",
                        var website: String = "",
                        var photoUrl: String,
-                       var isFavorite: Boolean = false)
+                        var isFavorite: Boolean = false) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as StoreEntity
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
