4efd4648bd51e8997d440736253d04f76e7f9898
Mariana Sanchez Gonzalez <mari98san33@gmail.com>
2021-12-12 01:03:04 -0600

    Validar TextFields en tiempo real

--- app/src/main/java/com/example/store/stores_da/EditStoreFragment.kt
+++ app/src/main/java/com/example/store/stores_da/EditStoreFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.*
 import android.text.Editable
 import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListene
 import androidx.fragment.app.Fragment
@@ -12,6 +13,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.stores_da.   databinding.FragmentEditStoreBinding
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
@@ -51,6 +53,9 @@ class EditStoreFragment : Fragment() {
                 .centerCrop()
                 .into(mBinding.imgPhoto)
         }
+        mBinding.etName.addTextChangedListener { validateFields(mBinding.tilName) }
+        mBinding.etPhone.addTextChangedListener { validateFields(mBinding.tilPhone) }
+        mBinding.etPhotoUrl.addTextChangedListener { validateFields(mBinding.tilPhotoUrl) }
     }
     private fun getStore(id: Long) {
         doAsync {
@@ -80,7 +85,8 @@ class EditStoreFragment : Fragment() {
                 true
             }
             R.id.action_save -> {
-                if (mStoreEntity != null && validateFields()){
+                if (mStoreEntity != null &&
+                    validateFields(mBinding.tilPhotoUrl,mBinding.tilPhone,mBinding.tilName)){
 /*val store = StoreEntity(name = mBinding.etName.text.toString().trim(),
 phone = mBinding.etPhone.text.toString().trim(),
 website = mBinding.etWebsite.text.toString().trim(),
@@ -120,6 +126,25 @@ else -> super.onOptionsItemSelected(item)
 //return super.onOptionsItemSelected(item)
 }
     private fun validateFields(): Boolean {
+        private fun validateFields(vararg textFields: TextInputLayout): Boolean{
+            var isValid = true
+
+            for (textField in textFields){
+                if (textField.editText?.text.toString().trim().isEmpty()){
+                    textField.error = getString(R.string.helper_required)
+                    textField.editText?.requestFocus()
+                    isValid = false
+                }
+            }else textField.error = null
+            }
+            if (!isValid) Snackbar.make(mBinding.root,
+                R.string.edit_store_message_valid,
+                Snackbar.LENGTH_SHORT).show()
+
+            return isValid
+        }
+
+    }
         var isValid = true
 
         if (mBinding.etPhotoUrl.text.toString().trim().isEmpty()){
